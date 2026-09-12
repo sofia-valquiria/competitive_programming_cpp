f569996ca3f7ae3a24c7e9ea06931161dd59fca9
sofia-valquiria <sofiavalquiriasandoval@gmail.com>
2026-09-12 15:26:50 -0300

    LA RE C*NCH* DE LA L*R* JAJJAJA estaba iterando mal; tarde 4 horas en darme cuenta q estaba haciendo str.len() iteraciones en vez de map.len() iteraciones ;_;

--- CF-R1062-Div4/b.cpp
+++ CF-R1062-Div4/b.cpp
@@ -7,36 +7,49 @@ int main(){
     cin >> q;
     bool esPosible = true;
 
-    unordered_map<char, int> ms;
+    unordered_map<char, int> ms; //diccionarios (arreglos con clave char)
     unordered_map<char, int> mt;
+
 
 
     for (int j = 0; j < q; j++){
         cin >> n;
         cin >> s >> t;
-        for(int i = 97; i < 123; i++){
+
+        for(int i = 97; i < 123; i++){ // (re)inicializo los diccionarios
             char c = i;
-            ms[c, 0];
-            mt[c, 0];
+            ms[c] = 0;
+            mt[c] = 0;                  // pq dsps necesito incrementarlos en 1
         }
 
-        for (int i = 0; i < n; i++){
-            char c = s[i];
+        for (int m = 0; m < n; m++){
+            char c = s[m];
             ms[c]++;
-            char d = t[i];
+            char d = t[m];
             mt[d]++;
         }
-
-        for(int i = 0; i < n; i++){
-            if(ms[i+97]!=mt[i+97]){
-                esPosible = false;
-                i = n;
+        for(int k = 97; k < 123; k++){ //aca taba el HDP error estuve como 4 horas total buscandote
+            if(ms[k]!= mt[k]){         // mis iteraciones se basaban en largo de palabra y no en
+                esPosible = false;     // largo del diccionario ;_;
             }
         }
 
         if(esPosible){
-            cout << "yes";
-        }else{ cout << "no";}
+            cout << "yes\n";
+        }else{ cout << "no\n";}
+        esPosible = true;
     }
     return 0;
 }
+/* test case:
+5
+7
+humitsa mitsuha
+4
+orhi hori
+6
+aakima makima
+6
+nezuqo nezuko
+6
+misaka mikasa */
